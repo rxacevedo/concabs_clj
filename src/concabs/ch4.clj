d8f73dfae04c22815d099acc936a34d8c792cf97
Roberto Acevedo <rxacevedo@gmail.com>
2013-11-18 06:34:18 -0500

    Finished some more questions for ch4.

--- src/concabs/ch4.clj
+++ src/concabs/ch4.clj
@@ -1,8 +1,6 @@
 (ns concabs.ch4
   (:gen-class)
-  (:require [clojure.math.numeric-tower :as math])
-  (:import [javax.swing JPanel JFrame]
-           [java.awt Dimension]))
+  (:require [clojure.math.numeric-tower :as math]))
 
 ;; Modular arithmetic
 ;; For each non-negative integer x, x mod m is the unique remainder r
@@ -76,8 +74,7 @@
 ;; in the text).
 (defn mod-expt-2 [b e m]
   "Modular exponentiation, linear iterative - O(1) memory/space, O(e) time"
-  (loop [a 1
-         e e]
+  (loop [a 1 e e]
     (if (zero? e) a
         (recur (mod* a b m) (dec e)))))
 
@@ -88,11 +85,9 @@
   (cond (zero? e) 1
         (even? e) (mod*
                    (mod-expt-3 b (/ e 2) m)
-                   (mod-expt-3 b (/ e 2) m)
-                   m)
+                   (mod-expt-3 b (/ e 2) m) m)
         :else (mod*
-               (mod-expt-3 b (dec e) m)
-               b m)))
+               (mod-expt-3 b (dec e) m) b m)))
 
 ;; This version is correct, it achieves a tree depth of log(e) and
 ;; uses memory proportional to log(e). This should be
@@ -100,148 +95,94 @@
 (defn mod-expt-4 [b e m]
   "Modular exponentiation, logarithmic recursion - O(log e) memory/space, O(log e) time"
   (cond (zero? e) 1
-        (even? e) (let [half (mod-expt-4 b (/ e 2) m)] ;; Beautiful.
+        (even? e) (let [half (mod-expt-4 b (/ e 2) m)]
                     (mod* half half m))
         :else (mod* (mod-expt-4 b (dec e) m) b m)))
 
-;; Exercise 4.4, the final, logarithmic time iterative version. e is decremented by
-;; 1 initially to prevent the extra multiplation step
-;; (b3 * (b1 * b1) * b0) <-- We do not want that 0 because when b2 is
-;; reduced to (b1 * b1) we have no multiplications left to do, so we
-;; settle for ((b1 * b1) * b0) instead
-;; (defn mod-expt [b e m]
-;;   "Modular exponentiation, logarithmic iteration - O(1) memory/space, O(log e) time"
-;;   (loop [a 1
-;;          e e]
-;;     (cond (zero? e) a
-;;           (even? e) (recur (mod* a (mod* b b m) m) (/ e 2))
-;;           :else (recur (mod* a b m) (dec e)))))
-
-;; (defn mod-expt-final-old [b e m]
-;;   (if (even? e)
-;;     (let [pair (mod* b b m)]
-;;       (loop [iter (/ e 2)
-;;              a 1]
-;;         (if (zero? iter) a
-;;             (recur (dec iter) (mod* a pair)))))
-;;       (mod* b (mod-expt-final b (dec e) m) m)))
-
-;; TODO: This is slower than mod-expt-4, why? Maybe consider what
-;; happens when (mod e 2) is zero.
 (defn mod-expt [b e m]
   "Modular exponentiation, logarithmic iteration - O(1) memory/space, O(log e) time"
-  (loop [a 1
-         e e
-         b b]
+  (loop [a 1 e e b b]
     (cond (zero? e) a
-          (even? e)
-          (let [iter (/ e 2)
-                p (mod* b b m)]
-            (recur (mod* a p m) (dec iter) p))
+          (even? e) (let [iter (/ e 2)
+                          p (mod* b b m)]
+                      (recur (mod* a p m) (dec iter) p))
           :else (recur (mod* a b m) (dec e) b))))
-
-;; Fractal stuff goes here
-
-;; (defn draw-rec [level]
-;;   (let [panel (proxy [JPanel] []
-;;                 (paintComponent [g]
-;;                   (loop [x1 0 y1 0 x2 100 y2 100 lvl level]
-;;                     (if (zero? lvl) 
-;;                       (.drawLine g x1 y1 x2 y2)
-;;                       (recur (inc x1) (inc y1) (inc x2) (inc y2) (dec level))))))]
-;;     (doto panel
-;;       (.setPreferredSize (Dimension. 400 400)))))
- 
-;; (defn make-frame [panel]
-;;   (doto (new JFrame)
-;;     (.add panel)
-;;     .pack
-;;     .show))
 
 ;; Excercise 4.12
 ;; There are three cases to consider for this exercise:
-;; i. When m^2 > n, there are 0 ways to factor n using numbers no
+;; (i). When m^2 > n, there are 0 ways to factor n using numbers no
 ;; smaller than m.
-;; ii. When n is not divisible by m, then the number of ways to factor
+;; (ii). When n is not divisible by m, then the number of ways to factor
 ;; n using numbers no smaller than m is the same as the number of ways
 ;; to factor n using numbers no smaller than m + 1 (because you are
 ;; using the same set of numbers to factor except you are leaving out
 ;; m, which was not a divisor of n anyways).
-;; iii. When m^2 <= n, there is at least one way to factor n using
+;; (iii). When m^2 <= n, there is at least one way to factor n using
 ;; numbers no smaller than m (m and n/m).
 (defn ways-to-factor-using-no-smaller-than [n m]
-  (loop [acc 0
-         x n
-         y m]
-       (cond (> (math/expt y 2) x) acc
-             (zero? (mod x y))
-                    (let [sub-factors (ways-to-factor-using-no-smaller-than (/ x y) y)]
-                              (println (str x " divides " y ", calculate factors of " (/ x y)))
-                              (recur (+ (inc acc) sub-factors) x (inc y)))
-          :else (recur acc x (inc y)))))
+  "Returns the number of distinct ways to factor n using numbers no smaller than m."
+  (loop [acc 0 a n b m]
+       (cond (> (math/expt b 2) a) acc
+             (zero? (mod a b))
+                    (let [sub-factors (ways-to-factor-using-no-smaller-than (/ a b) b)]
+                              (recur (+ (inc acc) sub-factors) a (inc b)))
+          :else (recur acc a (inc b)))))
 
 (defn ways-to-factor [n]
+  "Returns the number of distinct ways to factor n using numbers no smaller than 2."
   (ways-to-factor-using-no-smaller-than n 2))
 
 ;; Exercise 4.15
-;; (This is probably over-repetitious)
-(defn max-weighings [coll]
-  "Returns the number of weighings to determine which coin is fake (exercise 4.15)"
-  (loop [acc 0
-         curr_coll coll]
-    (cond (= 1 (count curr_coll)) acc
-          (even? (count curr_coll))
-          (let [mid (/ (count curr_coll) 2)
-                left (take mid curr_coll)
-                right (drop mid curr_coll)
-                lweight (reduce + left)
-                rweight (reduce + right)]
-            (prn left right)
-            (recur (inc acc) (if (< lweight rweight) left
-                                 right)))
-          :else
-          (let [[head & tail] curr_coll
-                mid (/ (count tail) 2)
-                left (take mid tail)
-                right (drop mid tail)
-                lweight (reduce + left)
-                rweight (reduce + right)]
-            (prn left right)
-            (recur (inc acc) (if (< lweight rweight) left
-                                 right))))))
+(defn max-weighings [& args]
+  "Returns the number of weighings to determine which coin is fake (exercise 4.15)."
+  (let [sum #(reduce + %)]
+    (loop [acc 0 coll args]
+      (let [sz (count coll)
+            r (mod sz 2)
+            psize (/ (- sz r) 2)]
+        (cond (= sz 1) acc
+              (zero? r) (let [[p1 p2] (partition psize coll)]
+                          (prn p1 p2)
+                          (if (< (sum p1) (sum p2)) (recur (inc acc) p1)
+                              (recur (inc acc) p2)))
+              :else     (let [[p1 p2] (partition psize (take (- sz r) coll))
+                              p3 (seq (drop (- sz r) coll))]
+                          (prn p1 p2 p3)
+                          (cond (< (sum p1) (sum p2)) (recur (inc acc) p1)
+                                (> (sum p1) (sum p2)) (recur (inc acc) p2)
+                                :else (recur (inc acc) p3))))))))
 
-;; TODO: This is NOT RIGHT, cannot always partition by 3, need to
-;; determine if modulus is 0 and if so then partition by (/ coll_sz 3;)
-(defn max-weighings-thirds [coll]
-  (loop [acc 0
-         curr_coll coll]
-    (Thread/sleep 100)
-    (let [sz (count curr_coll)
-          r (mod sz 3)
-          sum #(reduce + %)]
-      (println (str "r: " r))
-      (cond (or (= 1 sz) (nil? curr_coll)) acc
-            (zero? r) (let [psize (/ sz 3)
-                            [p1 p2 p3 :as all] (partition psize curr_coll)]
-                        (println "Divisible by 3")
-                        (prn curr_coll)
-                        (prn p1 p2 p3)
-                        (cond (> (sum p1) (sum p2)) (recur (inc acc) p2)
-                              (< (sum p1) (sum p2)) (recur (inc acc) p1)
-                              :else (recur (inc acc) p3)))
-            :else (let [psize (if (< (/ (- sz r) 3) 3) 1
-                                  (/ (- sz r) 3)) ;; Always divisible by 3
-                        [p1 p2] (partition psize (take (- sz r) curr_coll))
-                        ;; This becomes a mess when the piles are
-                        ;; smaller than 3...
-                        p3 (drop (* 2 psize) curr_coll)]
-                    (println "Not divisible by three")
-                    (println "psize was: " (str psize))
-                    (prn curr_coll)
-                    (prn p1 p2 p3)
-                    (cond (> (sum p1) (sum p2)) (recur (inc acc) p2)
-                          (< (sum p1) (sum p2)) (recur (inc acc) p1)
-                          :else (recur (inc acc) p3)))))))
+(defn max-weighings-thirds [& args]
+  "Returns the number of weighings required to determine which coin is fake.
+   This approach uses three piles instead of two."
+  (let [sum #(reduce + %)]
+    (loop [acc 0 coll args]
+      (let [sz (count coll)
+            r (mod sz 3)
+            psize (/ (- sz r) 3)]
+        (cond (= sz 1) acc
+              (= sz 2) (let [[p1 p2] (partition 1 coll)]
+                         (prn p1 p2)
+                         (if (< (sum p1) (sum p2)) (recur (inc acc) p1)
+                             (recur (inc acc) p2)))
+              (zero? r) (let [[p1 p2 p3] (partition psize coll)]
+                          (prn p1 p2 p3)
+                          (cond (< (sum p1) (sum p2)) (recur (inc acc) p1)
+                                (> (sum p1) (sum p2)) (recur (inc acc) p2)
+                                :else (recur (inc acc) p3)))
+              :else     (let [[p1 p2] (partition psize (take (* 2 psize) coll))
+                              p3 (drop (* 2 psize) coll)]
+                          (prn p1 p2 p3)
+                          (cond (< (sum p1) (sum p2)) (recur (inc acc) p1)
+                                (> (sum p1) (sum p2)) (recur (inc acc) p2)
+                                :else (recur (inc acc) p3))))))))
+
+;; Exercise 4.17, nCr/Pascal's triangle (r,c)
+(defn choose [n k]
+  "Returns the number of ways to choose k elements from a set of n elements."
+  (cond (or (zero? k) (= n k)) 1
+        (zero? n) 0
+        :else (+ (choose (dec n) (dec k)) (choose (dec n) k))))
 
 ;; Exercise 4.18
 (defn sum-ints [a b]
