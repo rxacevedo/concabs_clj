6778ba6f42678180c601b3558489452fc09d97a1
Roberto Acevedo <rxacevedo@gmail.com>
2013-12-02 07:07:09 -0500

    Work on Nim

--- src/concabs/ch6.clj
+++ src/concabs/ch6.clj
@@ -1,3 +1,7 @@
+(ns concabs.ch6
+  (:gen-class)
+  (:use [cemerick.pomegranate :only (add-dependencies)]))
+
 ;; Chapter 6 - Compound Data and Data Abstraction
 
 ;; Game state ADT
@@ -6,13 +10,21 @@
   [(vec (repeat n :c)) (vec (repeat m :c))])
 
 (defn size-of-pile [game-state pile-number]
+  ;; (println "In size-of-pile")
   (count (nth game-state pile-number)))
 
 (defn remove-coins-from-pile [game-state num-coins pile-number]
-  (let [pile (nth game-state pile-number)]
-    (if (> num-coins (size-of-pile game-state pile-number))
+  (let [[p0 p1] game-state
+        sz (size-of-pile game-state pile-number)]
+    ;; (println "In remove-coins")
+    (if (> num-coins sz)
       (throw (Exception. (str "Not enough coins in pile " pile-number " to remove " num-coins " coins.")))
-      (vec ()))))
+      (let [new-sz (- sz num-coins)]
+        (cond (= 0 pile-number) [(subvec p0 0 new-sz) p1]
+              (= 1 pile-number) [p0 (subvec p1 0 new-sz)]
+              :else (throw (Exception. "Crap")))))))
+
+(defn info [])
 
 (defn play-with-turns [game-state player]
   (info game-state))
